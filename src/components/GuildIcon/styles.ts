982008f203ed638335e418d582b71d0e87c95e9e
jose <jose.machoski@alexandria.solar>
2021-08-30 12:57:30 -0300

    App GamePlay feito na NLW Togheter

---
+++ src/components/GuildIcon/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+    container: {
+        height: 66,
+        width: 62,
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#7289da',
+        overflow: 'hidden'
+    },
+    image: {
+        height: 66,
+        width: 62,
+    },
+});
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Home } from '../screens/Home'
+import { AppointmentDetails } from '../screens/AppointmentDetails'
+import { AppointmentCreate } from '../screens/AppointmentCreate'
+import { SignIn } from '../screens/SignIn'
+
+import { theme } from "../global/styles/theme";
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function AppRoutes() {
+    return (
+        <Navigator
+            screenOptions={{
+                headerShown: false,
+                cardStyle: {
+                    backgroundColor: theme.colors.secondary100,
+                }
+            }}
+        >
+            <Screen
+                name="Home"
+                component={Home}
+            />
+            <Screen
+                name="AppointmentDetails"
+                component={AppointmentDetails}
+            />
+            <Screen
+                name="AppointmentCreate"
+                component={AppointmentCreate}
+            />
+        </Navigator>
+    )
+}
